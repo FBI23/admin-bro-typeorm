e9051406bbc7875391bba1957e2a087dc4907d3b
Yatin Badal <yatin@sov.tech>
2020-01-15 14:03:35 +0200

    fix: allow for graceful failure on type check

--- src/ExtendedRecord.ts
+++ src/ExtendedRecord.ts
@@ -3,55 +3,53 @@ import RecordJSON from "admin-bro/types/src/backend/decorators/record-json.inter
 import { BaseEntity } from "typeorm";
 import { Resource } from "./Resource";
 
-export class ExtendedRecord extends BaseRecord
-{
-    private _instance: BaseEntity | null = null;
-    private _customTitle: string | null = null;
+export class ExtendedRecord extends BaseRecord {
+  private _instance: BaseEntity | null = null;
+  private _customTitle: string | null = null;
 
-    constructor(instance: BaseEntity | null | undefined, resource: Resource)
-    {
-        super(instance, resource);
+  constructor(instance: BaseEntity | null | undefined, resource: Resource) {
+    super(instance, resource);
 
-        if(instance)
-            this._instance = instance;
-    }
+    if (instance) this._instance = instance;
+  }
 
-    public setTitle(value: string): void
-    {
-        this._customTitle = value;
-    }
+  public setTitle(value: string): void {
+    this._customTitle = value;
+  }
 
-    public title(): string
-    {
-        if(this._customTitle != null)
-            return this._customTitle;
+  public title(): string {
+    if (this._customTitle != null) return this._customTitle;
 
-        return super.title() || "";
-    }
+    return super.title() || "";
+  }
 
-    public toJSON(currentAdmin?: CurrentAdmin): RecordJSON
-    {
-        const obj = super.toJSON(currentAdmin);
-        obj.title = this.title(); // sorry but, .toJSON() doesn't take title from .title()
-        if(this._instance)
-        {
-            // patched strange objects ({"key.deepKey": 123}) to normal JSON.
-            obj.params = {};
-            for (const n in this._instance)
-            {
-                const value = this._instance[n];
-                const property = this.resource.property(n);
-                if(property)
-                {
-                    const type = property.type();
-                    if(type == "mixed")
-                        obj.params[n] = JSON.stringify(value);
-                    else
-                        obj.params[n] = value;
-                }
-            }
+  public toJSON(currentAdmin?: CurrentAdmin): RecordJSON {
+    const obj = super.toJSON(currentAdmin);
+    obj.title = this.title(); // sorry but, .toJSON() doesn't take title from .title()
+    if (this._instance) {
+      // patched strange objects ({"key.deepKey": 123}) to normal JSON.
+      obj.params = {};
+      for (const n in this._instance) {
+        const value = this._instance[n];
+        const property = this.resource.property(n);
+
+        if (property) {
+          console.log({ property });
+
+          let type;
+
+          try {
+            type = property.type();
+          } catch (error) {
+            type = "";
+          }
+
+          if (type == "mixed") obj.params[n] = JSON.stringify(value);
+          else obj.params[n] = value;
         }
-
-        return obj;
+      }
     }
+
+    return obj;
+  }
 }
